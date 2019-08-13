ecce20d432df44eb0cb2d9e9197b3cb6cbcd7d82
eduardo <eggduzao@gmail.com>
2019-08-13 23:13:42 +0200

    a

--- Results/GoldStandard/GoldStandard/runChipValidation.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-mpbsFileName="DU_H1hesc.bed"
-fpFileName="DU_H1hesc_HINTBC.bed"
-outputLocation="./"
-
-python chipValidation.py $mpbsFileName $fpFileName $outputLocation
-
-
--- Results/ROC/NatMet/Saos2/script.R
+++
@@ -1,9 +0,0 @@
-library(plotrix)
-cols=rainbow(6)
-styVec=c(1,1,1,1,1,1)
-segLen=c(rep(2,6),rep(.3,6))
-legendStyVec=c(styVec,rep(1,6))
-legendCol=c(cols,rep('white',6))
-mar.default <- c(5,5,4,2)
-source('/work/eg474423/eg474423_Projects/trunk/TfbsPrediction/Results/LabelTranslation/myLegend6.R')
-
